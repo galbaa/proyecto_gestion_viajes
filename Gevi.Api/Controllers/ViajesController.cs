63c9822bd2e7f2cdc0b076a5001acc7c800b2c35
Marcos Candia <MC222839@fi365.ort.edu.uy>
2020-01-11 18:38:40 -0300

    wip: deploy app remota

--- Gevi.Api/Controllers/ViajesController.cs
+++ Gevi.Api/Controllers/ViajesController.cs
@@ -15,130 +15,130 @@ namespace Gevi.Api.Controllers
 {
     public class ViajesController : ApiController
     {
-        /*private GeviApiContext db = new GeviApiContext();
+        //private GeviApiContext db = new GeviApiContext();
 
-        // GET: api/Viajes
-        public IQueryable<Viaje> GetViajes()
-        {
-            return db.Viajes;
-        }
+        //// GET: api/Viajes
+        //public IQueryable<Viaje> GetViajes()
+        //{
+        //    return db.Viajes;
+        //}
 
-        // GET: api/Viajes/5
-        [ResponseType(typeof(Viaje))]
-        public async Task<IHttpActionResult> GetViaje(int id)
-        {
-            Viaje viaje = await db.Viajes.FindAsync(id);
-            if (viaje == null)
-            {
-                return NotFound();
-            }
+        //// GET: api/Viajes/5
+        //[ResponseType(typeof(Viaje))]
+        //public async Task<IHttpActionResult> GetViaje(int id)
+        //{
+        //    Viaje viaje = await db.Viajes.FindAsync(id);
+        //    if (viaje == null)
+        //    {
+        //        return NotFound();
+        //    }
 
-            return Ok(viaje);
-        }
+        //    return Ok(viaje);
+        //}
 
-        // PUT: api/Viajes/5
-        [ResponseType(typeof(void))]
-        public async Task<IHttpActionResult> PutViaje(int id, Viaje viaje)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
+        //// PUT: api/Viajes/5
+        //[ResponseType(typeof(void))]
+        //public async Task<IHttpActionResult> PutViaje(int id, Viaje viaje)
+        //{
+        //    if (!ModelState.IsValid)
+        //    {
+        //        return BadRequest(ModelState);
+        //    }
 
-            if (id != viaje.Id)
-            {
-                return BadRequest();
-            }
+        //    if (id != viaje.Id)
+        //    {
+        //        return BadRequest();
+        //    }
 
-            db.Entry(viaje).State = EntityState.Modified;
+        //    db.Entry(viaje).State = EntityState.Modified;
 
-            try
-            {
-                await db.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!ViajeExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
+        //    try
+        //    {
+        //        await db.SaveChangesAsync();
+        //    }
+        //    catch (DbUpdateConcurrencyException)
+        //    {
+        //        if (!ViajeExists(id))
+        //        {
+        //            return NotFound();
+        //        }
+        //        else
+        //        {
+        //            throw;
+        //        }
+        //    }
 
-            return StatusCode(HttpStatusCode.NoContent);
-        }
+        //    return StatusCode(HttpStatusCode.NoContent);
+        //}
 
-        [HttpPost]
-        [Route("nuevo")]
-        public async Task<IHttpActionResult> NuevoViaje(ViajeRequest request)
-        {
-            if (!ModelState.IsValid)
-            {
-                return this.Content(HttpStatusCode.BadRequest, new HttpResponse<string>()
-                {
-                    StatusCode = 400,
-                    ApiResponse = new ApiResponse<string>()
-                    {
-                        Data = null,
-                        Error = new Error("El viaje que se intenta ingresar es invalido")
-                    }
-                });
-            }
-            var empleado = (Empleado)db.Usuarios.Where(u => u is Empleado && u.Id == request.EmpleadoId).FirstOrDefault();
-            var nuevo = new Viaje()
-            {
-                Empleado = empleado,
-                Estado = Estado.PENDIENTE_APROBACION,
-                FechaInicio = request.FechaInicio,
-                FechaFin = request.FechaFin,
-                Gastos = null,
-                Proyecto = null
-            };
-            db.Viajes.Add(nuevo);
-            await db.SaveChangesAsync();
+        //[HttpPost]
+        //[Route("nuevo")]
+        //public async Task<IHttpActionResult> NuevoViaje(ViajeRequest request)
+        //{
+        //    if (!ModelState.IsValid)
+        //    {
+        //        return this.Content(HttpStatusCode.BadRequest, new HttpResponse<string>()
+        //        {
+        //            StatusCode = 400,
+        //            ApiResponse = new ApiResponse<string>()
+        //            {
+        //                Data = null,
+        //                Error = new Error("El viaje que se intenta ingresar es invalido")
+        //            }
+        //        });
+        //    }
+        //    var empleado = (Empleado)db.Usuarios.Where(u => u is Empleado && u.Id == request.EmpleadoId).FirstOrDefault();
+        //    var nuevo = new Viaje()
+        //    {
+        //        Empleado = empleado,
+        //        Estado = Estado.PENDIENTE_APROBACION,
+        //        FechaInicio = request.FechaInicio,
+        //        FechaFin = request.FechaFin,
+        //        Gastos = null,
+        //        Proyecto = null
+        //    };
+        //    db.Viajes.Add(nuevo);
+        //    await db.SaveChangesAsync();
 
-            return this.Content(HttpStatusCode.Created, new HttpResponse<Viaje>()
-            {
-                StatusCode = 201,
-                ApiResponse = new ApiResponse<Viaje>()
-                {
-                    Data = nuevo,
-                    Error = null
-                }
-            });
-        }
+        //    return this.Content(HttpStatusCode.Created, new HttpResponse<Viaje>()
+        //    {
+        //        StatusCode = 201,
+        //        ApiResponse = new ApiResponse<Viaje>()
+        //        {
+        //            Data = nuevo,
+        //            Error = null
+        //        }
+        //    });
+        //}
 
-        // DELETE: api/Viajes/5
-        [ResponseType(typeof(Viaje))]
-        public async Task<IHttpActionResult> DeleteViaje(int id)
-        {
-            Viaje viaje = await db.Viajes.FindAsync(id);
-            if (viaje == null)
-            {
-                return NotFound();
-            }
+        //// DELETE: api/Viajes/5
+        //[ResponseType(typeof(Viaje))]
+        //public async Task<IHttpActionResult> DeleteViaje(int id)
+        //{
+        //    Viaje viaje = await db.Viajes.FindAsync(id);
+        //    if (viaje == null)
+        //    {
+        //        return NotFound();
+        //    }
 
-            db.Viajes.Remove(viaje);
-            await db.SaveChangesAsync();
+        //    db.Viajes.Remove(viaje);
+        //    await db.SaveChangesAsync();
 
-            return Ok(viaje);
-        }
+        //    return Ok(viaje);
+        //}
 
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
-        }
+        //protected override void Dispose(bool disposing)
+        //{
+        //    if (disposing)
+        //    {
+        //        db.Dispose();
+        //    }
+        //    base.Dispose(disposing);
+        //}
 
-        private bool ViajeExists(int id)
-        {
-            return db.Viajes.Count(e => e.Id == id) > 0;
-        }*/
+        //private bool ViajeExists(int id)
+        //{
+        //    return db.Viajes.Count(e => e.Id == id) > 0;
+        //}
     }
 }
